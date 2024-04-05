3a45d21f59e1c8b679b84b8348d341ad6b41cfaf
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-04-05 17:00:04 -0400

    implemented dark theme for login page

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <CssVarsProvider>
+        <App />
+      </CssVarsProvider>
+    </AuthContextProvider>
   </BrowserRouter>
 )
