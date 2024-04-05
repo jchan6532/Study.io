55cbe7e5401454e847125e21ce9a4ca0a9031884
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-04-05 03:58:37 -0400

    added study material storage module

---
+++ backend/services/StudyMaterialsStorage.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        const { userid } = req.params;
+        const uploadPath = path.join(__dirname, '..', 'uploads', 'study-materials', userid);
+
+        // Check if directory exists, create it if it doesn't
+        fs.mkdir(uploadPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error('Error creating directory:', err);
+                return cb(err);
+            }
+            else{
+                cb(null, uploadPath);
+            }
+        });
+    },
+    filename: function(req, file, cb) {
+        const newFilename = `${uuidv4()}${path.extname(file.originalname)}`;
+        cb(null, newFilename);
+    }
+});
+
+module.exports = storage;
