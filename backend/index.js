acf9bbddfd0228ad3173f1e94ea9284d052f688e
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-04-10 14:25:00 -0400

    config cors origin to allow everything

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,7 @@ socketIo.init(server);
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND,
-  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
